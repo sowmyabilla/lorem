46d92b68e15f75e0ad7e9450be7158453837d79b
sowmyabilla <sowmyabillakanti@gmail.com>
2022-02-24 11:07:17 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+    const text =[
+    `Lorem ipsum dolor sit amet. Autem facilis ut galisum Quis et debitis quia qui perferendis reiciendis sed nihil ullam et vero sunt. Ea dolores reprehenderit et totam ipsam ut fugiat veritatis. Ea sunt consequatur quo reiciendis repudiandae eos libero galisum qui praesentium placeat nam dolor commodi aut galisum dolore ut enim aperiam.`,
+
+    `Qui asperiores reiciendis et ducimus aliquid qui iusto consectetur. Ut nihil officia est excepturi consectetur a possimus animi ut vitae eligendi aut velit aperiam ut voluptatem eveniet autem asperiores. Aut mollitia veritatis hic doloremque error ut dolorem iusto non deserunt voluptatum. Non ipsum alias aut praesentium recusandae vel quia minima vel dolorem enim eum dicta corrupti ut consectetur totam?`,
+
+    `Est consequuntur nemo ut nemo quia aut sapiente quisquam qui nesciunt saepe. Ut expedita eaque qui assumenda eligendi ut ducimus commodi et internos voluptatem est enim maxime non facere provident! Et deserunt dolores 33 saepe praesentium ut cupiditate impedit ut eligendi optio quo quis sunt.`,
+
+    `Aut distinctio libero et reprehenderit rerum sit repudiandae optio vel laudantium nulla quo vitae eaque. Et nobis nihil qui dolorum nisi et repellendus perspiciatis est assumenda quisquam ut culpa eaque.`,
+
+    `Id consequatur expedita ut beatae necessitatibus sit nesciunt consequatur aut tenetur doloremque. In tenetur aliquid in mollitia distinctio ut omnis internos aut eveniet beatae aut doloribus ducimus. Est illo nesciunt sed officiis voluptatem nam provident tempora et delectus dolorum sed dolor sequi qui possimus eius cum neque corporis. Et delectus dignissimos ea nemo ipsum sequi sunt qui soluta ullam aut blanditiis saepe nam vitae quibusdam.`,
+
+    `Eos quibusdam quis in dicta commodi sed officia praesentium et consequatur sequi et optio amet eum cumque pariatur. Obcaecati enim eos voluptas quisquam eum repudiandae voluptatibus sed dolorem veritatis et nihil consequatur. Sit ipsum perferendis et ipsum sapiente ut alias minima ab natus soluta. Sit molestias sunt et autem libero et ducimus adipisci ut omnis nesciunt.`,
+
+    `Sit animi quia aut assumenda officiis eos quidem voluptas et suscipit possimus aut fugit dolore sit voluptates iusto. Aut alias dolorum id delectus consequatur qui modi quibusdam id neque distinctio. Id accusamus culpa in fugiat officiis hic dignissimos consequatur et eaque saepe et culpa molestias.`,
+
+    `Est temporibus temporibus ex tempore omnis et voluptatem consequatur. Est voluptatem doloribus et nostrum omnis sed amet. 33 porro laboriosam ad modi tempore sit perspiciatis sapiente!`,
+
+    `Ut praesentium explicabo neque ullam cum deserunt quia a numquam ipsam et deleniti nisi cum autem eligendi. Qui voluptatem debitis est delectus enim qui ipsa voluptatem.`,
+
+    `Est enim aperiam eos debitis consequuntur est ratione temporibus vel voluptas accusantium qui sint quae qui rerum ullam non totam minima. Quo voluptates assumenda nam assumenda fugiat est sint autem in necessitatibus impedit ad galisum quis sed fugiat fugiat ab eius beatae. Ut delectus inventore eos eaque vero et ipsam galisum quo fugiat dolores est ducimus. Est rerum libero est cupiditate facilis aut delectus eaque ut ipsum fugiat aut repudiandae nisi et exercitationem autem.`,
+];
+export default text;
